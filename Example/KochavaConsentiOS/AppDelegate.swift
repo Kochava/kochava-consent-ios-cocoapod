3f800a351bfb9693b63a88bc68f50ab1ae27af61
John Bushnell <jbushnell@kochava.com>
2021-04-21 13:04:41 -0700

    4.6.0

--- Example/KochavaConsentiOS/AppDelegate.swift
+++ Example/KochavaConsentiOS/AppDelegate.swift
@@ -24,10 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        // KVALog.shared.level = .info
+        // KVALog.shared.level
+        // ⓘ Set to trace.  This will result in the KVACoreProduct being registered, and this will be prefaced by any other known modules which are included.  This will be printed to the log where they can be inspected.
+        KVALog.shared.level = KVALogLevel.trace
         
-        print("KVACoreProduct.shared = \(KVACoreProduct.shared.kva_asForContextObject(withContext: .log)!)")
-
         // KVAConsentClient
         // 1. Register the Kochava deviceIdString as an identity.  Note that this is mock information.
         // 2. Start, now that we’ve registered at least one identity.
